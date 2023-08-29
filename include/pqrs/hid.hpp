27f0114c82eccc740048dfb3d2e29ce2b21300c7
Takayama Fumihiko <tekezo@pqrs.org>
2023-08-29 22:59:00 +0900

    http://www.boost.org -> https://www.boost.org

--- include/pqrs/hid.hpp
+++ include/pqrs/hid.hpp
@@ -4,7 +4,7 @@
 
 // (C) Copyright Takayama Fumihiko 2020.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "hid/country_code.hpp"
 #include "hid/hash.hpp"
