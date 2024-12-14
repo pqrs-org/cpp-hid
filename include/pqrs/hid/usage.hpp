82397f96f90573df7c07abd61aa457f2e2cca761
Indy Griffiths <hi@indy.nz>
2024-12-14 17:32:00 +1300

    Add `pqrs::hid::usage::consumer::bass_decrement`

--- include/pqrs/hid/usage.hpp
+++ include/pqrs/hid/usage.hpp
@@ -346,6 +346,7 @@ constexpr value_t mute(0x00e2);
 constexpr value_t volume_increment(0x00e9);
 constexpr value_t volume_decrement(0x00ea);
 constexpr value_t bass_boost(0x00e5);
+constexpr value_t bass_decrement(0x0153);
 
 // application launch buttons
 
